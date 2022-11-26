2516d4134e5a5956c0d390e0c073c1d874fa21a8
Rezzy Yuhand <rezzyyuhand92@student.ub.ac.id>
2022-11-27 01:20:58 +0700

    staging error lagi

--- src/main/java/com/basdat/controller/SignUpController.java
+++ src/main/java/com/basdat/controller/SignUpController.java
@@ -1,4 +1,16 @@
 package com.basdat.controller;
+import javafx.fxml.FXML;
+import java.io.IOException;
 
 public class SignUpController {
+    @FXML
+    private void signingUp() throws IOException {
+
+        try {
+
+        } catch (Exception e) {
+
+        }
+
+    }
 }
