d6e965d0412a55ef642770418a51fe1d100d5465
HanifRobby <96159381+HanifRobby@users.noreply.github.com>
2022-11-26 18:50:31 +0700

    Welcome FXML

---
+++ src/main/java/com/basdat/controller/WelcomeController.java
@@ -0,0 +1,18 @@
+package com.basdat.controller;
+
+import java.io.IOException;
+
+import com.basdat.repository.DBConnect;
+import javafx.fxml.FXML;
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+
+public class WelcomeController {
+
+
+
+}
