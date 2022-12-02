2c19d3003fb46749127075ff12ca9a1a683f8c4e
HanifRobby <96159381+HanifRobby@users.noreply.github.com>
2022-12-02 17:07:16 +0700

    Backup besar

--- src/main/java/com/basdat/controller/customer_controller/SignUpController.java
+++ src/main/java/com/basdat/controller/customer_controller/SignUpController.java
@@ -7,7 +7,14 @@ import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
+import javax.swing.*;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import static com.basdat.repository.DBConnect.connectionUrl;
 
 public class SignUpController {
     @FXML
@@ -24,21 +31,52 @@ public class SignUpController {
     private PasswordField fieldPasswordSU;
 
 
-    private String password;
+    private String email, user, password;
+
 
 
     @FXML
     private void signingUp() throws IOException {
         try {
+            email = fieldEmailSU.getText().trim();
+            user = fieldUsernameSU.getText().trim();
             password = fieldPasswordSU.getText().trim();
-            if (password.equals("")){
-                descriptionLabelSU.setText("Pass masih Kosong mas!!");
-            } else {
-                descriptionLabelSU.setText(password);
-                App.setRoot("fxml/welcome");
+            if (email.isBlank() || user.isBlank() || password.isBlank()){
+                descriptionLabelSU.setText("Please complete the fields above");
+            }
+            else {
+//                String cabang = NcTF.getText().trim();
+//                String table = "Pegawai";
+//                String table1 = "Pengguna";
+//
+//                String query = "INSERT INTO " + table + " values (?,?,?,?,?,?,?,?)";
+//                String query1 = "INSERT INTO " + table1 + " values (?,?,?,?)";
+//
+//                try(Connection connection = DriverManager.getConnection(connectionUrl);
+//                    Connection connection1 = DriverManager.getConnection(connectionUrl);
+//                    PreparedStatement ps = connection.prepareStatement(query);
+//                    PreparedStatement ps1 = connection1.prepareStatement(query1)) {
+//                    ps.setInt(1, Integer.parseInt(idPgn));
+//                    ps.setInt(2, Integer.parseInt(idPgw));
+//                    ps.setString(3, nama);
+//
+//
+//                    ps1.executeUpdate();
+//
+//                    ps.executeUpdate();
+//
+//                    JOptionPane.showMessageDialog(null, "ADD SUCCESS");
+//                }
+//                catch (SQLException e) {
+//                    e.printStackTrace();
+//                    JOptionPane.showMessageDialog(null, "ADD FAILED");
+//                }
+//
+//                descriptionLabelSU.setText("SIGN UP BERHASIL");
+//                App.setRoot("fxml/welcome");
             }
         } catch (Exception e) {
-
+            e.printStackTrace();
         }
     }
 
